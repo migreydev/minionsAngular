5ff891d914db59cc64986f021c79dc392f86d402
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2023-12-14 12:41:11 +0100

    Subiendo proyecto minion Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MinionComponent} from './minion/minion.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component'
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MinionComponent, NavBarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'minionAngular';
+  
+}
